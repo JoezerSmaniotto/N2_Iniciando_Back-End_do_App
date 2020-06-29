d425f53869aa81882c31a80e5572f1706864bd85
joezersmaniotto <joezersmaniotto5@gmail.com>
2020-06-29 15:09:48 -0300

    Criação de Upload de imagem model users, Service e alem disso no server a criação da rota para listar os arquivos que foram subidos atraves de uma rota static

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import UploadConfig from '../config/upload';
 
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(UploadConfig); // O upload é uma instancia do multer nele eu tenho acesso alguns metodos
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -21,5 +27,27 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+// Uso o ensureAuthenticated para ver se o usuario esta autenticado, caso que não acontece na rota acima pq ali ele estara logando
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'), // Aqui upload.single('avatar') acaba funcionando como um middleware, e no campo  avatar é nome do campo aonde estará minha imagem de upload
+  async (request, response) => {
+    try {
+      const updateUserService = new UpdateUserAvatarService();
+
+      const user = await updateUserService.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+      // delete user.password;
+
+      return response.json({ user });
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default usersRouter;
